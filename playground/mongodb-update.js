bda30c08f728b44aafc67b3b03784f9e0bad3215
Jason He <qhe1995@gmail.com>
2018-10-22 21:46:25 -0400

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,39 @@
+// const MongoClient = require('mongodb').MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server')
+  }
+  console.log('Connected to MongoDB server')
+  const db = client.db('TodoApp')
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5bce77ab7eeb1873ea0139d0')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result)
+  // })
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5bce72397eeb1873ea013906')
+  }, {
+    $set: {
+      name: 'Jason'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result)
+  })
+
+  client.close()
+})
